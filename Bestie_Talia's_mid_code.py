2b6de57201bdf8b7f5681edd89ec3cbd7d55aaf8
Kashani,Talia <90413899+Taliaistyping@users.noreply.github.com>
2023-03-17 14:23:50 -0700

    Create Bestie_Talia's_mid_code.py

---
+++ Bestie_Talia's_mid_code.py
@@ -0,0 +1,9 @@
+def choice(round_score, my_score, opponent_score):
+    if my_score <= 30:
+        return True
+    else:
+        return False
+    if opponent_score <= 2:
+        return True
+    else:
+        return False
